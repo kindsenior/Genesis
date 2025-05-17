2e4e9bd44795daf9cd85cedb0fb075f9310563f6
kindsenior <kunio.einstein@gmail.com>
2025-05-17 19:13:12 +0900

    add log_dir option to go2_train and go2_eval

--- examples/locomotion/go2_eval.py
+++ examples/locomotion/go2_eval.py
@@ -24,13 +24,14 @@ from go2_env import Go2Env
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--exp_name", type=str, default="go2-walking")
+    parser.add_argument("-l", "--log_dir", type=str, default="logs")
     parser.add_argument("--ckpt", type=int, default=100)
     args = parser.parse_args()
 
     gs.init()
 
-    log_dir = f"logs/{args.exp_name}"
-    env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg = pickle.load(open(f"logs/{args.exp_name}/cfgs.pkl", "rb"))
+    log_dir = f"{args.log_dir}/{args.exp_name}"
+    env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg = pickle.load(open(f"{log_dir}/cfgs.pkl", "rb"))
     reward_cfg["reward_scales"] = {}
 
     env = Go2Env(
--- examples/locomotion/go2_train.py
+++ examples/locomotion/go2_train.py
@@ -148,13 +148,14 @@ def get_cfgs():
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--exp_name", type=str, default="go2-walking")
+    parser.add_argument("-l", "--log_dir", type=str, default="logs")
     parser.add_argument("-B", "--num_envs", type=int, default=4096)
     parser.add_argument("--max_iterations", type=int, default=101)
     args = parser.parse_args()
 
     gs.init(logging_level="warning")
 
-    log_dir = f"logs/{args.exp_name}"
+    log_dir = f"{args.log_dir}/{args.exp_name}"
     env_cfg, obs_cfg, reward_cfg, command_cfg = get_cfgs()
     train_cfg = get_train_cfg(args.exp_name, args.max_iterations)
 
